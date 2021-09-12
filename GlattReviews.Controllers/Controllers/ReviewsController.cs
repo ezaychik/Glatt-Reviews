77a69038dff5207e7c22fa332fa64da532211a84
Elya Zaychik <ezaychik@gmail.com>
2021-09-12 22:18:49 +0300

    add controllers

---
+++ GlattReviews.Controllers/Controllers/ReviewsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GlattReviews.API.Controllers
+{
+    [Route("[controller]")]
+    public class ReviewsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetReviews()
+        {
+            try
+            {
+                return Ok(new { Id = 1, Name = "Some Review" });
+
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+    }
+}
